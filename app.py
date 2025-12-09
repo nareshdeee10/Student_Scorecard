a1d86e0ec7c3314cbe41885d1cdd8a2c2390dee4
Naresh Kumar <50436121+nareshdeee10@users.noreply.github.com>
2025-12-09 20:20:59 +0530

    Change page icon in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 # ==================== PAGE CONFIG ====================
 st.set_page_config(
     page_title="ABC School • Results 2025",
-    page_icon="chart_with_upwards_trend",
+    page_icon="icon.png",
     layout="wide"
 )
 
